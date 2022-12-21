57316b6a08e9094aa736050b14cd1a43d2cfe71a
Irina <irina.m.protasevich@gmail.com>
2022-12-21 13:46:36 +0400

    feat:add solution to Task02 'Decision making statement'

---
+++ src/lesson03/Task02.java
@@ -0,0 +1,28 @@
+package lesson03;
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число:" );
+        n = Integer.parseInt(in.nextLine());
+        if (n == 1) {
+            System.out.println("Понедельник");
+        } else if (n == 2) {
+            System.out.println("Вторник");
+        } else if (n == 3) {
+            System.out.println("Среда");
+        } else if (n == 4) {
+            System.out.println("Четверг");
+        } else if (n == 5) {
+            System.out.println("Пятница");
+        } else if (n == 6) {
+            System.out.println("Суббота");
+        } else if (n == 7) {
+            System.out.println("Воскресенье");
+        } else {
+            System.out.println("Некорректный ввод");
+        }
+    }
+}
